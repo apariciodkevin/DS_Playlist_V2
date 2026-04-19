e8697545bfaad4d0dcc105c35588fd9ba4f7d334
KevnAp <apariciodkevin@Gmail.com>
2026-04-19 01:15:57 -0500

    finished most methods

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,8 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        File mp3File = new File("audio/20 Cigarettes--Morgan Wallen.mp3");
+
+       /* File mp3File = new File("audio/20 Cigarettes--Morgan Wallen.mp3");
         if (Desktop.isDesktopSupported()) {
             Desktop desktop = Desktop.getDesktop();
             if (desktop.isSupported(Desktop.Action.OPEN)) {
@@ -21,6 +22,44 @@ public class Main {
                     e.printStackTrace(); // object that describes what went wrong if something went wrong.
                 }
             }
-        }
+        } */
+        SongNode song1 = new SongNode("20 Cigaretttes", "Morgan Wallen", "../audio/20 Cigarettes--Morgan Wallen.mp3");
+        SongNode song2 = new SongNode("All the Love", "Ye", "../audio/All the love--Ye.mp3");
+        SongNode song3 = new SongNode("American Girls", "Harry Styles", "../audio/American Girls--Harry Styles.mp3");
+        SongNode song4 = new SongNode("E85", "Don Toliver", "../audio/E85--Don Toliver.mp3");
+        SongNode song5 = new SongNode("Hooligan", "BTS", "../audio/Hooligan--BTS.mp3");
+        SongNode song6 = new SongNode("Risk it All", "Bruno Mars", "../audio/Risk it all--Bruno Mars.mp3");
+        SongNode song7 = new SongNode("Seeing Someong", "Luke Combs", "../audio/Seeing Someone--Luke Combs.m-3");
+        SongNode song8 = new SongNode("So Easy", "Olivia Dean", "../audio/SoEasy--Olivia Dean.mp3");
+        SongNode song9 = new SongNode("Saint James Infirmary", "Louis Armstrong", "../audio/song1.mp3");
+        SongNode song10 = new SongNode("Turista", "BadBunny", "../audio/Turista--Bad Bunny.mp3");
+
+
+
+        Playlist playlist = new Playlist();
+        playlist.displayPlaylist();
+        playlist.addSong(song1);
+        playlist.displayPlaylist();
+        playlist.addSong(song4);
+        playlist.addSong(song7);
+        playlist.displayPlaylist();
+        playlist.addSong(song2);
+        playlist.getPlaying();
+        System.out.println(playlist.getSize());
+        System.out.println(playlist.getPlaying().getSongName());
+        playlist.nextSong();
+        System.out.println(playlist.getPlaying().getSongName());
+        playlist.nextSong();
+        playlist.nextSong();
+        System.out.println(playlist.getPlaying().getSongName());
+        playlist.prevSong();
+        System.out.println(playlist.getPlaying().getSongName());
+
+
+
+
+
+
+
     }
 }
--- src/Playlist.java
+++ src/Playlist.java
@@ -1,2 +1,82 @@
 public class Playlist {
+
+    private SongNode head;
+    private SongNode tail;
+    private int size;
+    private SongNode playing;
+
+    public Playlist(){ // Constructor
+        head = null;
+        tail = null;
+        size = 0;
+        playing = null;
+    }
+
+    // Methods
+
+    public int getSize(){
+        return size;
+    }
+    public void addSong(SongNode song){
+        if (size == 0){ // if the list is empty
+            head = song;
+            playing = song;
+            tail = song;
+            size++;
+        } else {
+            song.setPrev(tail);
+            tail.setNext(song);
+            tail = song;
+            size++;
+        }
+    }
+    public void displayPlaylist(){
+        if (size == 0){ // If there are no songs in the playlist
+            System.out.println("Playlist is EMPTY");
+        } else {
+            SongNode song = head;
+            int counter = 1;
+            while (song != null){
+                String title = song.getSongName();
+                String artist = song.getArtist();
+                System.out.println(counter + ". " + title + " - " + artist);
+                counter++;
+                song = song.getNext();
+
+            }
+        }
+    }
+
+    public SongNode getPlaying(){
+        return playing;
+    }
+
+    public SongNode nextSong(){
+        if (size == 0){
+            return null;
+        }
+        else if (playing.getNext() != null) {
+            playing = playing.getNext();
+        } else {
+                playing = head;
+            }
+        return playing;
+    }
+    public SongNode prevSong(){
+        if (size == 0){
+            return null;
+        }
+        else if (playing.getPrev() != null){
+            playing = playing.getPrev();
+        } else {
+            playing = tail;
+        }
+        return playing;
+
+    }
+    public void removeSong(){
+
+    }
+
+
 }
